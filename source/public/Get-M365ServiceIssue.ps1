fb028bd3a600ddffd9e98467398bea9e392c0d68
June Castillote <tito.castillote-jr@dxc.com>
2024-08-15 15:05:11 +0800

    Update Get-M365ServiceIssue.ps1

--- source/public/Get-M365ServiceIssue.ps1
+++ source/public/Get-M365ServiceIssue.ps1
@@ -3,18 +3,20 @@ Function Get-M365ServiceIssue {
     param (
 
         [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [string]
         $Id,
 
         [parameter()]
+        [ValidateRange(1, [int]::MaxValue)]
         [int]$PastDays,
 
         [parameter()]
-        [datetime]$LastUpdatedTime,
+        [datetime]$LastModifiedDateTime,
 
-        [parameter()]
-        [ValidateSet('Resolved', 'Unresolved')]
-        [string]$Status,
+        [Parameter()]
+        [bool]
+        $IsResolved,
 
         [parameter()]
         [ValidateSet('Advisory', 'Incident')]
@@ -29,39 +31,53 @@ Function Get-M365ServiceIssue {
     $filter = @()
 
     # Terminate if the -Id parameter is used with other parameters.
-    if ($id -and ($PastDays -or $LastUpdatedTime -or $Status -or $Classification -or $Service)) {
+    if ($PSBoundParameters.ContainsKey('Id') -and (
+            $PSBoundParameters.ContainsKey('PastDays') -or
+            $PSBoundParameters.ContainsKey('LastModifiedDateTime') -or
+            $PSBoundParameters.ContainsKey('Status') -or
+            $PSBoundParameters.ContainsKey('Classification') -or
+            $PSBoundParameters.ContainsKey('Service')
+        )) {
         Write-Error "The -Id parameter must be used alone."
         return $null
     }
 
-    # Terminate if -LastUpdatedTime and -PastDays are both in use.
-    if ($LastUpdatedTime -and $PastDays) {
-        Write-Error "The -LastUpdatedTime and -PastDays parameters cannot be used together."
+    # Terminate if -LastModifiedDateTime and -PastDays are both in use.
+    if ($PSBoundParameters.ContainsKey('LastModifiedDateTime') -and $PSBoundParameters.ContainsKey('PastDays')) {
+        Write-Error "The -LastModifiedDateTime and -PastDays parameters cannot be used together."
         return $null
     }
 
-    if ($Id) {
+    # Add Id filter
+    if ($PSBoundParameters.ContainsKey('Id')) {
         $filter += "Id eq '$($Id)'"
     }
 
-    if ($PastDays) {
+    # Add PastDays filter
+    if ($PSBoundParameters.ContainsKey('PastDays')) {
         $start_date = (([datetime]::Today).AddDays(-$PastDays)).ToString('yyyy-MM-ddTHH:mm:ssZ')
         $filter += "LastModifiedDateTime ge $($start_date)"
     }
 
-    if ($LastUpdatedTime) {
-        $start_date = ($LastUpdatedTime).ToString('yyyy-MM-ddTHH:mm:ssZ')
+    # Add LastModifiedDateTime filter
+    if ($PSBoundParameters.ContainsKey('LastModifiedDateTime')) {
+        $start_date = ($LastModifiedDateTime).ToString('yyyy-MM-ddTHH:mm:ssZ')
     }
 
-    if ($Classification) {
-        $filter += "Classification eq '$($Classification.ToLower())'"
+    # Add Classification filter
+    if ($PSBoundParameters.ContainsKey('Classification')) {
+        $filter += "Classification eq '$($Classification)'"
     }
 
-    switch ($Status) {
-        Resolved { $filter += 'IsResolved eq true' }
-        Unresolved { $filter += 'IsResolved eq false' }
+    # Add IsResolved filter
+    if ($PSBoundParameters.ContainsKey('IsResolved')) {
+        switch ($IsResolved) {
+            $true { $filter += 'IsResolved eq true' }
+            $false { $filter += 'IsResolved eq false' }
+        }
     }
 
+    # Add Service filter
     if ($Service) {
         $isValid = $true
         $valid_service_list = @((Get-MgServiceAnnouncementHealthOverview -All | Sort-Object Service).Service)
@@ -82,15 +98,34 @@ Function Get-M365ServiceIssue {
         $filter += "($($service_filter -join ' or '))"
     }
 
-    switch ($true) {
-        { $filter } {
-            Write-Verbose ($filter -join ' and ')
-            $issue_collection = Get-MgServiceAnnouncementIssue -Filter ($filter -join ' and ') -All
+    try {
+        switch ($true) {
+            { $filter } {
+                # Get issues with filter
+                Write-Verbose ($filter -join ' and ')
+                $issue_collection = Get-MgServiceAnnouncementIssue -Filter ($filter -join ' and ') -All
+            }
+            { !$filter } {
+                # Get issues without filter
+                $issue_collection = Get-MgServiceAnnouncementIssue -All
+            }
         }
-        { !$filter } {
-            $issue_collection = Get-MgServiceAnnouncementIssue -All
-        }
-    }
 
-    $issue_collection
+        $issue_collection | Add-Member -MemberType NoteProperty -Name LastUpdateContent -Value ''
+        $issue_collection | ForEach-Object {
+            # Split the Status and convert to title case (ie. 'serviceDegradation' to 'Service Degradation')
+            $_.Status = ($_.Status.substring(0, 1).toupper() + $_.Status.substring(1) -creplace '[^\p{Ll}\s]', ' $&').Trim()
+            # Capitalize the first letter of the Origin (ie. 'microsoft' to 'Microsoft')
+            $_.Origin = ($_.Origin.substring(0, 1).toupper() + $_.Origin.substring(1))
+            # Capitalize the first letter of the Classification (ie. 'advisory' to 'Advisory')
+            $_.Classification = ($_.Classification.substring(0, 1).toupper() + $_.Classification.substring(1))
+            # Bring out the latest message from the Posts collection.
+            $_.LastUpdateContent = $_.Posts[-1].Description.Content
+        }
+        # Return the results
+        $issue_collection
+    }
+    catch {
+        Write-Error "Failed to retrieve service health messages. $_"
+    }
 }
